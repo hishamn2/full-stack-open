9674cb9f74ef89bc9fbf033eb22d5fa4ebd574c7
Hicham Naouchi <hishamnaoushi@gmail.com>
2026-04-23 18:23:12 +0300

    completed part b

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,27 +2,62 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
-const HandleNameChange = (event) => {
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter,setNewFilter] =useState('')
+  const HandleNumberChange = (event) =>{
+  setNewNumber(event.target.value)
+  } 
+  const HandleNameChange = (event) => {
   setNewName(event.target.value)
 }
+const HandleFilterChange = (event) =>{
+  setNewFilter(event.target.value)
+}
+
 const addName = (event) => {
-  event.preventDefault()
-   const nameObject ={ name :newName}
-   setPersons(persons.concat(nameObject))
-   setNewName('')
+    event.preventDefault()
+    
+    // Check if the name exists
+    const nameExists = persons.some(person => person.name === newName)
+    const numberExists=persons.some(person => person.number===newNumber)
 
-
-   }
+    // The if...else logic
+    if (nameExists ) {
+      alert(`${newName} is already added to phonebook`)
+    }else if(numberExists){
+      alert(`${newNumber} is already added to phonebook`)
+    }
+     else {
+      const nameObject = { name: newName, number: newNumber }
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setNewNumber('')
+      setNewFilter('')
+    }
+  }
+  const personsToShow= persons.filter(person => person.name.toLowerCase().startsWith(newFilter.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with :<input value={newFilter} onChange={HandleFilterChange} />
+        </div>
+      </form>
+      <h2>add a new </h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={HandleNameChange} />
+          name: <input required type="text" value={newName} onChange={HandleNameChange} />
+        </div>
+        <div>
+          number: <input required type="number"value={newNumber}  onChange={HandleNumberChange} />
         </div>
         <div>
           <button type="submit" >add</button>
@@ -30,8 +65,8 @@ const addName = (event) => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+        {personsToShow.map((person) => (
+          <li key={person.name}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
