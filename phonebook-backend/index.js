585bcfa2343d238a02f7150c678195e65ab1ff02
Hicham Naouchi <hishamnaoushi@gmail.com>
2026-05-05 11:18:43 +0300

    3.2

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -26,7 +26,10 @@ let persons= [
 app.get('/api/persons',( request,response) =>{
     response.json(persons)
 })
-
+app.get('/info',(request,response)=>{
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
 const PORT=3001
 app.listen(PORT,() =>{
     console.log(`server is running on port ${PORT}`)
